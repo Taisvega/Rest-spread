d4cddfc0a111d4eadbca93261a3986c05464f57d
Tais Vega <taisvega.y@gmail.com>
2023-05-07 22:45:28 -0700

    object

---
+++ Object Enhancements Exercise/object.js
@@ -0,0 +1,37 @@
+function createInstructor(firstName, lastName){
+    return {
+      firstName,
+      lastName
+    }
+  }
+
+//   let favoriteNumber = 42;
+
+//   const instructor = {
+//     firstName: "Colt",
+//     [favoriteNumber]: "That is my favorite!"
+//   }
+
+  const instructor = {
+    firstName: "Colt",
+    sayHi() {
+      return "Hi!";
+    },
+    sayBye() {
+      return this.firstName + " says bye!";
+    }
+  }
+  
+  const dog = createAnimal("dog", "bark", "woof");
+  const sheep = createAnimal("sheep", "bleet", "BAAAAaaaa");
+ 
+  
+  function createAnimal(species, verb, noise) {
+    return {
+      species,
+      [verb]() {
+        return noise;
+      }
+    }
+  }
+  
